c05f9dcbd2957d32c83ad6bdfe53a8f874950efa
Arya Lesmana <arya.lesmana21@ui.ac.id>
2023-09-06 15:59:21 +0700

    Menambahakan folder main

--- shopping_list/urls.py
+++ shopping_list/urls.py
@@ -1,3 +1,5 @@
+from django.urls import path, include
+
 """
 URL configuration for shopping_list project.
 
@@ -15,8 +17,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', include('main.urls'))
 ]
